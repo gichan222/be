112e9276bc640f9fcfd5ed4d480ff0fb39eb0b41
Kichan Kim <repitor15@gmail.com>
2026-03-24 17:05:47 +0900

    Update GoogleOauthController.java

--- auth/src/main/java/be/auth/controller/GoogleOauthController.java
+++ auth/src/main/java/be/auth/controller/GoogleOauthController.java
@@ -59,7 +59,7 @@ public class GoogleOauthController {
 			ResponseCookie.from("refreshToken", result.refreshToken())
 				.httpOnly(true)
 				// TODO : 배포 서비스에서는 true를 사용
-				.secure(false)          // HTTPS 환경에서만
+				.secure(true)          // HTTPS 환경에서만
 				.sameSite("NONE")
 				.path("/")
 				.maxAge(Duration.ofDays(14))
@@ -94,7 +94,7 @@ public class GoogleOauthController {
 			"Set-Cookie",
 			ResponseCookie.from("refreshToken", "")
 				.httpOnly(false)
-				.secure(false)    // 배포 시 true
+				.secure(true)    // 배포 시 true
 				.sameSite("NONE")
 				.path("/")
 				.maxAge(0)
